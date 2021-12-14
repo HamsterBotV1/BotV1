c93281ee127ca0458c24d3e96c47e2e1e76c9d74
HamsterBotV1 <96092137+HamsterBotV1@users.noreply.github.com>
2021-12-14 17:03:19 +0700

    Add files via upload

---
+++ help.js
@@ -0,0 +1,38 @@
+module.exports.config = {
+	name: "help",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Hamster",
+	description: "Hướng dẫn cho người mới",
+	commandCategory: "system",
+	usages: "[Tên module]",
+	cooldowns: 5,
+	envConfig: {
+		autoUnsend: true,
+		delayUnsend: 10
+	}
+};
+
+module.exports.languages = {
+	"vi": {
+		"moduleInfo": "「 %1 」\n%2\n\n❯ Cách sử dụng: %3\n❯ Thuộc nhóm: %4\n❯ Thời gian chờ: %5 giây(s)\n❯ Quyền hạn: %6\n\n» Module code by %7 «",
+		"helpList": '[ Hiện tại đang có %1 lệnh có thể sử dụng trên bot này, Sử dụng: "%2help nameCommand" để xem chi tiết cách sử dụng! ]"',
+		"user": "Người dùng",
+        "adminGroup": "Quản trị viên nhóm",
+        "adminBot": "Quản trị viên bot"
+	},
+	"en": {
+		"moduleInfo": "「 %1 」\n%2\n\n❯ Usage: %3\n❯ Category: %4\n❯ Waiting time: %5 seconds(s)\n❯ Permission: %6\n\n» Module code by %7 «",
+		"helpList": '[ There are %1 commands on this bot, Use: "%2help nameCommand" to know how to use! ]',
+		"user": "User",
+        "adminGroup": "Admin group",
+        "adminBot": "Admin bot"
+	}
+}
+
+module.exports.handleEvent = function ({ api, event, getText }) {
+	const { commands } = global.client;
+	const { threadID, messageID, body } = event;
+
+	if (!body || typeof body == "undefined" || body.indexOf("help") != 0) return;
+	const splitBody = body.slice(body.indexOf("help")).trim().split(/\s+/);
