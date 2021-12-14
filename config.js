15cdd0c5ca2bf723db9c6062cd1b5b5931306e91
HamsterBotV1 <96092137+HamsterBotV1@users.noreply.github.com>
2021-12-14 08:28:32 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,31 @@
+{
+    "language":"vn",
+    "keyActive":"",
+    "DeveloperMode": false,
+    "autoCreateDB": true,
+    "allowInbox": true,
+    "autoClean": false,
+    "commandDisabled": [],
+    "eventDisabled": [],
+    "Email": "",
+    "Password"; "",
+    "OTPKEY": "",
+    "BOTNAME": "",
+    "PREFIX": "🐹",
+    "ADMINBOT": [
+         "ID1",
+         "iD2"
+ ],
+ "DATABASE": {
+      "sqlite": {
+          "storage": "data.sqlite"
+      }
+ },
+ "FBSTATEPATH": "fbstate.json",
+ "FCAOption": {
+      "forcealogin": true,
+      "listenEvents": true,
+      "pauseLog": "error",
+      "selfListen": false,
+      "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win.64; x64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/90.0.4430.72 Safari/537.36"
+ }
